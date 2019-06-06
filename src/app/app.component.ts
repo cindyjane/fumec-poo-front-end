0f8406f15a5ace486a38b09590f7bb8152645c3e
Cindy Silva <cindysilva@iheartmedia.com>
2019-06-06 17:22:58 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'siscom-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SisCom';
+}
